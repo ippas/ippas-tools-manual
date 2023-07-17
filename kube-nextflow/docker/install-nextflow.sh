9bf8698425f26cc2412f68e0cd11ee3fa3ee12fd
Jacek Hajto <72321150+20minadventure@users.noreply.github.com>
2023-07-17 12:08:37 +0000

    Update nextflow image - bash as default shell for screen

--- kube-nextflow/docker/install-nextflow.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-curl -s "https://get.sdkman.io" | bash
-source "/root/.sdkman/bin/sdkman-init.sh" && sdk install java 17.0.6-amzn
-cd /usr/bin
-wget -qO- "https://get.nextflow.io" | bash
